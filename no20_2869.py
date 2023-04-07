a3f663de0b04fbe2bf5a54c55dd54ef91cd49074
bigperson <daein7076@gmail.com>
2023-04-08 02:16:00 +0900

    solve No.22 9020

--- no20_2869.py
+++ no20_2869.py
@@ -1,13 +1,5 @@
 import math
 
 A, B, V = map(int, input().split())
-# days = 0
-# while True:
-#     days += 1
-#     V -= A
-#     if V <= 0:
-#         break
-#     V += B
-# print(days)
 
 print(math.ceil((V - A) / (A - B)) + 1)
